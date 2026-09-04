5925224e29765cc8f83ecdbd9f8400a6ef52b2d0
Aya Ragab <ayarsk4343@gmail.com>
2026-09-04 11:22:55 +0300

    [82] [Time Beats: 100.00%] [Memory Beats: 54.73%] - LeetPush

--- 82-Remove-Duplicates-from-Sorted-List-II.py
+++ 82-Remove-Duplicates-from-Sorted-List-II.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#         val = val
+#         next = next
 class Solution(object):
     def deleteDuplicates(self, head):
         """
@@ -11,35 +11,22 @@ class Solution(object):
         """
         if not head or not head.next:
             return head
-        curr = head
+        prev, curr = None, head
         while curr:
             val = curr.val
             temp = curr
             while temp and temp.val == val:
                 temp = temp.next
             if curr.next != temp:
-                if curr == head and temp and not temp.next:
-                    head = temp
-                    break
-                if curr == head and not temp:
-                    head = None
-                    break
-                if curr != head and not temp:
-                    curr2 = head
-                    while curr2.next != curr:
-                        curr2 = curr2.next
-                    curr2.next = None
-                    break
-                if curr != head and temp:
-                    curr2 = head
-                    while curr2.next != curr:
-                        curr2 = curr2.next
-                    curr2.next = temp
-                    curr = temp
-                    continue
-                if curr == head and temp and temp.next:
+                if curr == head:
                     head = temp
                     curr = temp
+                    prev = None
+                    continue     
+                else:
+                    prev.next = temp   
+                    curr = temp
                     continue
+            prev = curr
             curr = curr.next
         return head
